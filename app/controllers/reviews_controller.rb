e1e6a99c87dcc36948d4a37e2ab0c2994119c19a
tieunhanbioi <doiphongba@gmail.com>
2016-11-10 18:45:24 -0500

    Link Reviews with Restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :set_restaurant
   before_action :authenticate_user!
 
 
@@ -17,6 +18,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.restaurant_id = @restaurant.id
 
     respond_to do |format|
       if @review.save
@@ -58,6 +60,10 @@ class ReviewsController < ApplicationController
     def set_review
       @review = Review.find(params[:id])
     end
+
+    def set_restaurant 
+      @restaurant = Restaurant.find(params[:restaurant_id])
+    end 
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
