a4a913994656d4a5bfe02adc60a706c20247bd77
tieunhanbioi <doiphongba@gmail.com>
2016-11-10 00:49:42 -0500

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :reviews, dependent: :destroy       
+  has_many :review, dependent: :destroy       
 end
